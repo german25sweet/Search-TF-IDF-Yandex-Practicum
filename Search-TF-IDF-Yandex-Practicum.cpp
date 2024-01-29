52b8d5a55fbd8200f56780a358d306cc88f8deb9
German <german25avenue@gmail.com>
2024-01-29 17:00:27 +0300

    Добавил метод MatchDocument

--- Search-TF-IDF-Yandex-Practicum.cpp
+++ Search-TF-IDF-Yandex-Practicum.cpp
@@ -144,6 +144,36 @@ public:
 		return matched_documents2;
 	}
 
+	tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query, int document_id) const
+	{
+		auto query = ParseQuery(raw_query);
+
+		set<string> matched_strings;
+
+		if (!query.minus_words_.count(document_id))
+		{
+			for (auto& query_word : query.query_words_)
+			{
+				if (documents_.count(query_word) > 0)
+				{
+					try {
+						if (documents_.at(query_word).at(document_id))
+							matched_strings.insert(query_word);
+					}
+
+					catch (std::out_of_range ex)
+					{
+						continue;
+					}
+				}
+			}
+		}
+
+		vector <string> matched_words_vector{ matched_strings.begin(), matched_strings.end() };
+
+		return tuple(matched_words_vector, document_ratings_and_status.at(document_id).status);
+	}
+
 private:
 
 	struct DocumentAttributes {
@@ -246,12 +276,15 @@ SearchServer CreateSearchServer() {
 	return search_server;
 }
 
-void PrintDocument(const Document& document) {
+void PrintMatchDocumentResult(int document_id, const vector<string>& words, DocumentStatus status) {
 	cout << "{ "s
-		<< "document_id = "s << document.id << ", "s
-		<< "relevance = "s << document.relevance << ", "s
-		<< "rating = "s << document.rating
-		<< " }"s << endl;
+		<< "document_id = "s << document_id << ", "s
+		<< "status = "s << static_cast<int>(status) << ", "s
+		<< "words ="s;
+	for (const string& word : words) {
+		cout << ' ' << word;
+	}
+	cout << "}"s << endl;
 }
 int main() {
 	SearchServer search_server;
@@ -260,14 +293,9 @@ int main() {
 	search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, { 7, 2, 7 });
 	search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, { 5, -12, 2, 1 });
 	search_server.AddDocument(3, "ухоженный скворец евгений"s, DocumentStatus::BANNED, { 9 });
-	cout << "ACTUAL:"s << endl;
-
-	for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, DocumentStatus::ACTUAL)) {
-		PrintDocument(document);
+	const int document_count = search_server.GetDocumentCount();
+	for (int document_id = 0; document_id < document_count; ++document_id) {
+		const auto [words, status] = search_server.MatchDocument("пушистый кот"s, document_id);
+		PrintMatchDocumentResult(document_id, words, status);
 	}
-	cout << "BANNED:"s << endl;
-	for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, DocumentStatus::BANNED)) {
-		PrintDocument(document);
-	}
-	return 0;
 }
