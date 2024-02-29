ba6c576f5b41ef155c277b743d13bedb9818614d
German <german25avenue@gmail.com>
2024-02-29 19:03:32 +0300

    правки для Main

--- Search-TF-IDF-Yandex-Practicum.cpp
+++ Search-TF-IDF-Yandex-Practicum.cpp
@@ -333,20 +333,43 @@ void PrintDocument(const Document& document) {
 int main() {
 	setlocale(LC_ALL, "Russian");
 
-	try
-	{
+	try {
 		// SearchServer search_server("и \x0A в на"s);
 		SearchServer search_server;
 		search_server.FindTopDocuments("--прив");
 	}
 
-	catch (invalid_argument& ex)
-	{
+	catch (invalid_argument& ex) {
 		cout << "Ошибка - " << ex.what() << endl;
 	}
-	catch (out_of_range& ex)
-	{
+	catch (out_of_range& ex) {
 		cout << "Ошибка - " << ex.what() << endl;
 	}
-	
+
+	try {
+		SearchServer search_server("и в на"s);
+		search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, { 8, -3 });
+		search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, { 7, 2, 7 });
+		search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, { 5, -12, 2, 1 });
+		search_server.AddDocument(3, "ухоженный скворец евгений"s, DocumentStatus::BANNED, { 9 });
+		cout << "ACTUAL by default:"s << endl;
+		for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s)) {
+			PrintDocument(document);
+		}
+		cout << "BANNED:"s << endl;
+		for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, DocumentStatus::BANNED)) {
+			PrintDocument(document);
+		}
+		cout << "Even ids:"s << endl;
+		for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, [](int document_id, DocumentStatus status, int rating) { return document_id % 2 == 0; })) {
+			PrintDocument(document);
+		}
+	}
+
+	catch (invalid_argument& ex) {
+		cout << "Ошибка - " << ex.what() << endl;
+	}
+	catch (out_of_range& ex) {
+		cout << "Ошибка - " << ex.what() << endl;
+	}	
 }
